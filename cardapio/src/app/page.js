d26267f2212622127863dd62eaadd45fe4d3a385
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-27 21:12:45 -0300

    projeto finalizado

--- cardapio/src/app/page.js
+++ cardapio/src/app/page.js
@@ -1,56 +1,56 @@
-'use client'
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import estilos from "./page.module.css";
 import Banner from "/public/banner.png";
 import { useState } from "react";
-import estilos from "./page.module.css";
 import {
   filtrarProdutos,
-  buscarProdutos,
+  buscarProduto,
   produtosEntradas,
 } from "./servico";
-import Cards from "../componentes/Cards";
-import Categorias from "../componentes/Categorias";
-import CampoDeBusca from "../componentes/CampoDeBusca";
+import Cards from '../componentes/Cards';
+import Categorias from '../componentes/Categorias';
+import CampoDeBusca from '../componentes/CampoDeBusca';
 
 export default function Home() {
-   
+
   const [dadosFiltrados, setDadosFiltrados] = useState(produtosEntradas);
   const [textoBuscaDigitado, setTextoBuscaDigitado] = useState("");
-  const [botaoClicado, setBotaoClicado] = useState ("Entradas");
+  const [botaoClicado, setBotaoClicado] = useState("Entradas");
 
-  const HandleBusca = (textoDigitado) => {
+  const handleBusca = (textoDigitado) => {
     setTextoBuscaDigitado(textoDigitado);
-    textoDigitado.length >= 3 && setDadosFiltrados(buscarProdutos(textoDigitado));
+    textoDigitado.length >= 3 && setDadosFiltrados(buscarProduto(textoDigitado));
     setBotaoClicado("");
-  }
+  };
 
   const handleFiltro = (categoria) => {
     setTextoBuscaDigitado("");
     setDadosFiltrados(filtrarProdutos(categoria));
+    setBotaoClicado(categoria);
   };
 
   return (
     <>
-      <header className={estilos.estilos_topo}>
-        <Image src={Banner} alt="banner"/>
+      <header className={estilos.topo}>
+        <Image src={Banner} alt="banner" />
         <div>
-          <h1>
-            RESTAURANT
-          </h1>
+          <h1>RESTAURANT</h1>
           <p>
-            De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.
+            De pratos clássicos a criações surpreendentes, nosso cardápio é um
+            requinte de sabores refinados.
           </p>
         </div>
       </header>
       <main className={estilos.container_principal}>
-        <Categorias 
+        <Categorias
           handleFiltro={handleFiltro}
           botaoClicado={botaoClicado}
         />
 
         <CampoDeBusca
           textoBuscaDigitado={textoBuscaDigitado}
-          HandleBusca={HandleBusca}
+          handleBusca={handleBusca}
         />
 
         <section className={estilos.secao_cards}>
@@ -62,7 +62,6 @@ export default function Home() {
           </div>
         </section>
       </main>
-    
     </>
-  );
+  )
 }
